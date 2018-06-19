2a4252445fd59f343ad10db66df267400b21ec92
Jaci Holak <jaci@multimicrosystems.com>
2018-06-19 10:51:40 -0400

    ADDED Button2_Click EVENT

--- GitDemo/Form2.vb
+++ GitDemo/Form2.vb
@@ -2,4 +2,8 @@
     Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         MessageBox.Show("HELLO FROM FORM2")
     End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("HELLO FROM BUTTON 2")
+    End Sub
 End Class
